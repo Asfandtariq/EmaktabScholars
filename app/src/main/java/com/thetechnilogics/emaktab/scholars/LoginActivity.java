bfa01005d05a60e0615c154597d44bac96a35961
Asfand <muhammadasfandtariq@gmail.com>
2019-02-26 17:35:22 +0500

    Scholars Project

--- app/src/main/java/com/thetechnilogics/emaktab/scholars/LoginActivity.java
+++ app/src/main/java/com/thetechnilogics/emaktab/scholars/LoginActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,36 +20,40 @@ import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
 
-//    @BindView(R.id.login_submit_btn)
-//    Button login_submit_btn;
-//
-//    @BindView(R.id.login_phone_editbox)
-//    EditText login_phone_editbox;
-//
-//    @BindView(R.id.login_password_editbox)
-//    EditText login_password_editbox;
-//    Activity activity;
+    @BindView(R.id.login_submit_btn)
+    Button login_submit_btn;
+
+    @BindView(R.id.login_phone_editbox)
+    EditText login_phone_editbox;
+
+    @BindView(R.id.login_password_editbox)
+    EditText login_password_editbox;
+    Activity activity;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        this.activity=this;
-        setContentView(R.layout.attendence_layout);
-//        ButterKnife.bind(this);
-//        DiagonalView diagonalView = (DiagonalView)findViewById(R.id.diagonal);
-//
-//        diagonalView.setScaleType(ImageView.ScaleType.FIT_XY);
+       this.activity=this;
+        setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+
+
+
 
 
     }
 
-//    @OnClick(R.id.login_submit_btn)
-//    public void login_btn(View view){
-//        Toast.makeText(activity,"Login Button",Toast.LENGTH_SHORT).show();
-//        Intent intent=new Intent(activity,MainActivity.class);
-//        startActivity(intent);
-//
-//
-//    }
+    @OnClick(R.id.login_submit_btn)
+    public void login_btn(View view){
+        Toast.makeText(activity,"Login Button",Toast.LENGTH_SHORT).show();
+        Intent intent=new Intent(activity,MainActivity.class);
+        startActivity(intent);
+
+
+    }
 
 }
